fda1a5d78cc648c15b6a7cb29c5a43512ee49d4a
Giovani Simon <gio.simon@hotmail.com>
2022-03-13 19:58:32 -0300

    base spring project for backend

---
+++ BackEnd/EasyTimesheet/src/main/java/com/ufrgs/EasyTimesheet/EasyTimesheetApplication.java
@@ -0,0 +1,13 @@
+package com.ufrgs.EasyTimesheet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyTimesheetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyTimesheetApplication.class, args);
+	}
+
+}
---
+++ BackEnd/EasyTimesheet/src/test/java/com/ufrgs/EasyTimesheet/EasyTimesheetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ufrgs.EasyTimesheet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyTimesheetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
