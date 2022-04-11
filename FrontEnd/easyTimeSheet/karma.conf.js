d9faabbf08064c41e06cab75475318349b1765c5
Gabriel Martins <gabrielmartins173@gmail.com>
2022-04-11 00:31:18 -0300

    updating new version for front

--- FrontEnd/easyTimeSheet/karma.conf.js
+++ FrontEnd/easyTimeSheet/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/easy-time-sheet'),
+      dir: require('path').join(__dirname, './coverage/EasyTimeSheet'),
       subdir: '.',
       reporters: [
         { type: 'html' },
