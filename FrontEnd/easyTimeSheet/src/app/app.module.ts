8161afb9bff49fafcfca70fb7a9fb276f2b2ff66
Gabriel Martins <gabrielmartins173@gmail.com>
2022-04-30 18:32:03 -0300

    removing unnecessary endpoints

--- FrontEnd/easyTimeSheet/src/app/app.module.ts
+++ FrontEnd/easyTimeSheet/src/app/app.module.ts
@@ -9,9 +9,6 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { HomeComponent } from './home/home.component';
 import { ProfileComponent } from './profile/profile.component';
-import { BoardAdminComponent } from './board-admin/board-admin.component';
-import { BoardModeratorComponent } from './board-moderator/board-moderator.component';
-import { BoardUserComponent } from './board-user/board-user.component';
 
 import { authInterceptorProviders } from './_helpers/auth.interceptor';
 import { ConfirmPresenceComponent } from './confirm-presence/confirm-presence.component';
@@ -61,9 +58,6 @@ import { TeacherClassesComponent } from './teacher-classes/teacher-classes.compo
     RegisterComponent,
     HomeComponent,
     ProfileComponent,
-    BoardAdminComponent,
-    BoardModeratorComponent,
-    BoardUserComponent,
     ConfirmPresenceComponent,
     ListOfClassroomsComponent,
     TeacherDashboardComponent,
